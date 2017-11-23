a7f37386b629a6382bc357585b99fd40f69ae870
Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
2017-11-23 17:46:26 +0100

    Add documentation

--- engine/src/main/java/de/digitalcollections/workflow/engine/model/Message.java
+++ engine/src/main/java/de/digitalcollections/workflow/engine/model/Message.java
@@ -1,11 +1,31 @@
 package de.digitalcollections.workflow.engine.model;
 
+/**
+ * A generic message as it will be sent over RabbitMQ.
+ *
+ * @param <ID> The type of the identifier field.
+ */
 public interface Message<ID> {
 
+  /**
+   * Technical metadata like timestamps and retries.
+   *
+   * @return An object containing the messages technical metadata.
+   */
   Meta getMeta();
 
+  /**
+   * The optional message type is represented by an arbitrary string value to distinguish different events on the same queue.
+   *
+   * @return The message type.
+   */
   String getType();
 
+  /**
+   * The optional ID can hold the identifier of an corresponding object (e.g. a book to be indexed).
+   *
+   * @return The corresponding object's id.
+   */
   ID getId();
 
 }
--- engine/src/main/java/de/digitalcollections/workflow/engine/model/Meta.java
+++ engine/src/main/java/de/digitalcollections/workflow/engine/model/Meta.java
@@ -2,6 +2,9 @@ package de.digitalcollections.workflow.engine.model;
 
 import java.time.LocalDateTime;
 
+/**
+ * Technical metadata all implementations of {@link Message} must have.
+ */
 public class Meta {
 
   private String body;
@@ -12,34 +15,81 @@ public class Meta {
 
   private LocalDateTime timestamp;
 
+  /**
+   * Default constructor setting the Meta.timestamp to now.
+   */
+  public Meta() {
+    timestamp = LocalDateTime.now();
+  }
+
+  /**
+   * The original String representation before serializing into an {@link Message} instance. This field will not be serialized when the message is sent.
+   *
+   * @return The original String representation of the {@link Message} as received from RabbitMQ.
+   */
   public String getBody() {
     return body;
   }
 
+  /**
+   * The original String representation before serializing into an {@link Message} instance. This field will not be serialized when the message is sent.
+   *
+   * @param body The string representation to set.
+   */
   public void setBody(String body) {
     this.body = body;
   }
 
+  /**
+   * The delivery tag set by RabbitMQ for low level message handling.
+   *
+   * @return The delivery tag set by RabbitMQ.
+   */
   public long getDeliveryTag() {
     return deliveryTag;
   }
 
+  /**
+   * The delivery tag set by RabbitMQ for low level message handling.
+   *
+   * @param deliveryTag The delivery tag set by RabbitMQ.
+   */
   public void setDeliveryTag(long deliveryTag) {
     this.deliveryTag = deliveryTag;
   }
 
+  /**
+   * Gets the number of retries before a message is sent to the failed queue.
+   *
+   * @return The number of retries.
+   */
   public int getRetries() {
     return retries;
   }
 
+  /**
+   * Sets the number of retries before a message is sent to the failed queue.
+   *
+   * @param retries The number of retries.
+   */
   public void setRetries(int retries) {
     this.retries = retries;
   }
 
+  /**
+   * Gets the timestamp when the {@link Message} was created.
+   *
+   * @return The timestamp when the message was created.
+   */
   public LocalDateTime getTimestamp() {
     return timestamp;
   }
 
+  /**
+   * Sets the timestamp when the {@link Message} was created.
+   *
+   * @param timestamp The timestamp when the message was created.
+   */
   public void setTimestamp(LocalDateTime timestamp) {
     this.timestamp = timestamp;
   }
