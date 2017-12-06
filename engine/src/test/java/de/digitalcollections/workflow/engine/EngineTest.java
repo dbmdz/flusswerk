b20913f88e8ab984185b0d51f5a7873786805d63
Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
2017-12-06 11:18:19 +0100

    Add multithreading test

--- engine/src/test/java/de/digitalcollections/workflow/engine/EngineTest.java
+++ engine/src/test/java/de/digitalcollections/workflow/engine/EngineTest.java
@@ -10,24 +10,23 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import static de.digitalcollections.workflow.engine.model.DefaultMessage.withType;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class EngineTest {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(EngineTest.class);
 
   private static final String IN = "in";
 
@@ -57,10 +56,9 @@ class EngineTest {
         .build();
   }
 
-  @Disabled("Test does not work in Travis CI")
   @Test
   public void engineShouldUseMaxNumberOfWorkers() throws IOException, InterruptedException {
-    when(messageBroker.receive(any())).thenReturn(DefaultMessage.withType("White Room"));
+    when(messageBroker.receive()).thenReturn(DefaultMessage.withType("White Room"));
 
     Semaphore semaphore = new Semaphore(1);
     semaphore.drainPermits();
@@ -69,7 +67,9 @@ class EngineTest {
         .read(Message::getType)
         .transform(s -> {
           try {
+            LOGGER.debug("Trying to acquire semaphore, should block (Thread id {})", Thread.currentThread().getId());
             semaphore.acquire(); // Block this worker to count it only once
+            LOGGER.debug("Got semaphore (Thread id {})", Thread.currentThread().getId());
           } catch (InterruptedException e) {
             e.printStackTrace();
           }
@@ -89,42 +89,42 @@ class EngineTest {
     }
 
     assertThat(engine.getActiveWorkers()).isEqualTo(engine.getConcurrentWorkers());
+    engine.stop();
   }
 
-  @Disabled("Test results are sometimes wrong")
-  @Test
-  public void engineShouldSendMessageToOut() throws IOException, InterruptedException {
-    when(messageBroker.receive(any())).thenReturn(withType("White Room"));
-
-    AtomicInteger messagesSent = new AtomicInteger();
-
-    Flow<Message, String, String> flow = new FlowBuilder<Message, String, String>()
-        .read(Message::getType)
-        .transform(s -> {
-          messagesSent.incrementAndGet();
-          return s;
-        })
-        .write(DefaultMessage::withType)
-        .build();
-
-    Engine engine = new Engine(messageBroker, flow);
-    ExecutorService executorService = Executors.newSingleThreadExecutor();
-    executorService.submit(engine::start);
-
-    int millisecondsWaited = 0;
-    while (messagesSent.get() == 0 && millisecondsWaited < 250) {
-      millisecondsWaited += 1;
-      TimeUnit.MILLISECONDS.sleep(1);
-    }
-
-    ArgumentCaptor<String> routingCaptor = ArgumentCaptor.forClass(String.class);
-    ArgumentCaptor<Message> messageCaptor = ArgumentCaptor.forClass(Message.class);
-    verify(messageBroker, atLeastOnce()).send(routingCaptor.capture(), messageCaptor.capture());
-
-    assertThat(routingCaptor.getValue()).isEqualTo("out");
-    assertThat(messageCaptor.getValue().getType()).isEqualTo("White Room");
-    System.out.println(messagesSent.get());
-  }
+//  @Test
+//  public void engineShouldSendMessageToOut() throws IOException, InterruptedException {
+//    when(messageBroker.receive(any())).thenReturn(withType("White Room"));
+//
+//    AtomicInteger messagesSent = new AtomicInteger();
+//
+//    Flow<Message, String, String> flow = new FlowBuilder<Message, String, String>()
+//        .read(Message::getType)
+//        .transform(s -> {
+//          messagesSent.incrementAndGet();
+//          return s;
+//        })
+//        .write(DefaultMessage::withType)
+//        .build();
+//
+//    Engine engine = new Engine(messageBroker, flow);
+//    ExecutorService executorService = Executors.newSingleThreadExecutor();
+//    executorService.submit(engine::start);
+//
+//    int millisecondsWaited = 0;
+//    while (messagesSent.get() == 0 && millisecondsWaited < 250) {
+//      millisecondsWaited += 1;
+//      TimeUnit.MILLISECONDS.sleep(1);
+//    }
+//
+//    ArgumentCaptor<String> routingCaptor = ArgumentCaptor.forClass(String.class);
+//    ArgumentCaptor<Message> messageCaptor = ArgumentCaptor.forClass(Message.class);
+//    verify(messageBroker, atLeastOnce()).send(routingCaptor.capture(), messageCaptor.capture());
+//
+//    assertThat(routingCaptor.getValue()).isEqualTo("out");
+//    assertThat(messageCaptor.getValue().getType()).isEqualTo("White Room");
+//    System.out.println(messagesSent.get());
+//  }
 
   private final Function<Message, String> READ_SOME_STRING = Message::getType;
 
