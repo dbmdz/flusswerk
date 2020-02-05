b91a21b79acadbd0f3c473575f17f36626d54b79
Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
2020-02-05 11:29:28 +0100

    Format code

--- spring-boot-starter-flusswerk/src/main/java/de/digitalcollections/flusswerk/spring/boot/starter/FlusswerkConfiguration.java
+++ spring-boot-starter-flusswerk/src/main/java/de/digitalcollections/flusswerk/spring/boot/starter/FlusswerkConfiguration.java
@@ -12,15 +12,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-/**
- * Spring configuration to provide beans for{@link MessageBroker} and {@link Engine}.
- */
+/** Spring configuration to provide beans for{@link MessageBroker} and {@link Engine}. */
 @Configuration
 @Import(FlusswerkPropertiesConfiguration.class)
 public class FlusswerkConfiguration {
 
   /**
-   * @param flusswerkProperties   The external configuration from <code>application.yml</code>
+   * @param flusswerkProperties The external configuration from <code>application.yml</code>
    * @param messageImplementation A custom {@link Message} implementation to use.
    * @return The message broker for this job.
    */
@@ -45,7 +43,7 @@ public class FlusswerkConfiguration {
       builder.connectTo(connection.getConnectTo());
     } else {
       throw new IllegalArgumentException("connect-to is missing");
-    };
+    }
 
     if (connection.getVirtualHost() != null) {
       builder.virtualHost(connection.getVirtualHost());
@@ -75,19 +73,18 @@ public class FlusswerkConfiguration {
           }
         });
 
-
     return builder.build();
   }
 
   /**
-   * @param messageBroker         The messageBroker to use.
-   * @param flowProvider          The flow to use (optional).
-   * @param flusswerkProperties   The external configuration from <code>application.yml</code>.
+   * @param messageBroker The messageBroker to use.
+   * @param flowProvider The flow to use (optional).
+   * @param flusswerkProperties The external configuration from <code>application.yml</code>.
    * @param processReportProvider A custom process report provider (optional).
-   * @param <I>                   The message's identifier type.
-   * @param <M>                   The used {@link Message} type
-   * @param <R>                   The type of the reader implementation
-   * @param <W>                   The type of the writer implementation
+   * @param <I> The message's identifier type.
+   * @param <M> The used {@link Message} type
+   * @param <R> The type of the reader implementation
+   * @param <W> The type of the writer implementation
    * @return The {@link Engine} used for this job.
    * @throws IOException If connection to RabbitMQ fails permanently.
    */
