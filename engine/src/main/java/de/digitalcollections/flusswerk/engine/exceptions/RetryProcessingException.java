e43ff31365b5d777e03ed9f8bd2aee4415c33728
Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
2020-02-28 14:29:23 +0100

    Extract supplier class

--- engine/src/main/java/de/digitalcollections/flusswerk/engine/exceptions/RetryProcessingException.java
+++ engine/src/main/java/de/digitalcollections/flusswerk/engine/exceptions/RetryProcessingException.java
@@ -3,13 +3,10 @@ package de.digitalcollections.flusswerk.engine.exceptions;
 import java.util.Optional;
 import java.util.function.Supplier;
 
-/**
- * Throw this exception in case of errors that might go away when trying again later.
- */
+/** Throw this exception in case of errors that might go away when trying again later. */
 public class RetryProcessingException extends RuntimeException {
 
   /**
-   *
    * @param message The message, possibly including format strings
    * @param args Arguments for the format string in <code>message</code>
    */
@@ -23,6 +20,7 @@ public class RetryProcessingException extends RuntimeException {
 
   /**
    * Add the exceptions cause to the exception without interfering with the format string arguments.
+   *
    * @param cause The cause to add
    * @return An augmented version of the exception including the cause
    */
@@ -31,45 +29,15 @@ public class RetryProcessingException extends RuntimeException {
   }
 
   /**
-   * Fluid interface to create exception suppliers, e.g. for use in {@link Optional#orElseThrow(Supplier)}.
+   * Fluid interface to create exception suppliers, e.g. for use in {@link
+   * Optional#orElseThrow(Supplier)}.
+   *
    * @param message The message, possibly including format strings
    * @param args Arguments for the format string in <code>message</code>
    * @return A supplier for this exception than can be further customized
    */
-  public static RetryProcessingExceptionSupplier because(String message, Object... args) {
-    return new RetryProcessingExceptionSupplier(message, args);
-  }
-
-  /**
-   * A supplier for RetryProcessingException, e.g. for use in {@link Optional#orElseThrow(Supplier)}.
-   */
-  public static class RetryProcessingExceptionSupplier
-      implements Supplier<RetryProcessingException> {
-
-    private String message;
-
-    private Object[] args;
-
-    private Throwable cause;
-
-    public RetryProcessingExceptionSupplier(String message, Object[] args) {
-      this.message = message;
-      this.args = args;
-    }
-
-    @Override
-    public RetryProcessingException get() {
-      return new RetryProcessingException(String.format(message, args), cause);
-    }
-
-    /**
-     * Add the exceptions cause to the supplier without interfering with the format string arguments.
-     * @param cause The cause to add
-     * @return An augmented version of the supplier including the cause
-     */
-    public RetryProcessingExceptionSupplier causedBy(Throwable cause) {
-      this.cause = cause;
-      return this;
-    }
+  public static ExceptionSupplier<RetryProcessingException> because(
+      String message, Object... args) {
+    return new ExceptionSupplier<>(RetryProcessingException.class, message, args);
   }
 }
--- engine/src/main/java/de/digitalcollections/flusswerk/engine/exceptions/StopProcessingException.java
+++ engine/src/main/java/de/digitalcollections/flusswerk/engine/exceptions/StopProcessingException.java
@@ -3,13 +3,10 @@ package de.digitalcollections.flusswerk.engine.exceptions;
 import java.util.Optional;
 import java.util.function.Supplier;
 
-/**
- * Throw this exception in case of errors that won't go away when trying again later.
- */
+/** Throw this exception in case of errors that won't go away when trying again later. */
 public class StopProcessingException extends RuntimeException {
 
   /**
-   *
    * @param message The message, possibly including format strings
    * @param args Arguments for the format string in <code>message</code>
    */
@@ -23,6 +20,7 @@ public class StopProcessingException extends RuntimeException {
 
   /**
    * Add the exceptions cause to the exception without interfering with the format string arguments.
+   *
    * @param cause The cause to add
    * @return An augmented version of the exception including the cause
    */
@@ -31,45 +29,15 @@ public class StopProcessingException extends RuntimeException {
   }
 
   /**
-   * Fluid interface to create exception suppliers, e.g. for use in {@link Optional#orElseThrow(Supplier)}.
+   * Fluid interface to create exception suppliers, e.g. for use in {@link
+   * Optional#orElseThrow(Supplier)}.
+   *
    * @param message The message, possibly including format strings
    * @param args Arguments for the format string in <code>message</code>
    * @return A supplier for this exception than can be further customized
    */
-  public static StopProcessingExceptionSupplier because(String message, Object... args) {
-    return new StopProcessingExceptionSupplier(message, args);
-  }
-
-  /**
-   * A supplier for RetryProcessingException, e.g. for use in {@link Optional#orElseThrow(Supplier)}.
-   */
-  public static class StopProcessingExceptionSupplier
-      implements Supplier<StopProcessingException> {
-
-    private String message;
-
-    private Object[] args;
-
-    private Throwable cause;
-
-    public StopProcessingExceptionSupplier(String message, Object[] args) {
-      this.message = message;
-      this.args = args;
-    }
-
-    @Override
-    public StopProcessingException get() {
-      return new StopProcessingException(String.format(message, args), cause);
-    }
-
-    /**
-     * Add the exceptions cause to the supplier without interfering with the format string arguments.
-     * @param cause The cause to add
-     * @return An augmented version of the supplier including the cause
-     */
-    public StopProcessingExceptionSupplier causedBy(Throwable cause) {
-      this.cause = cause;
-      return this;
-    }
+  public static ExceptionSupplier<StopProcessingException> because(
+      String message, Object... args) {
+    return new ExceptionSupplier<>(StopProcessingException.class, message, args);
   }
 }
