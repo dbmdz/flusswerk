d47d39123a806c602bdf5a0021c1f18856635a5d
Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
2020-06-05 12:45:33 +0200

    Simplify FlusswerkConfigurationTest

--- spring-boot-starter/src/test/java/com/github/dbmdz/flusswerk/spring/boot/starter/FlusswerkConfigurationTest.java
+++ spring-boot-starter/src/test/java/com/github/dbmdz/flusswerk/spring/boot/starter/FlusswerkConfigurationTest.java
@@ -2,37 +2,28 @@ package com.github.dbmdz.flusswerk.spring.boot.starter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+@DisplayName("FlusswerkConfiguration should")
 public class FlusswerkConfigurationTest {
 
-  @Test
-  public void isSetShouldDetectNull() {
-    assertThat(FlusswerkConfiguration.isSet(null)).isFalse();
+  private static Stream<Arguments> isSetSource() {
+    return Stream.of(
+        Arguments.of("null", null, false),
+        Arguments.of("Empty String", "", false),
+        Arguments.of("Whitespace", "  \t ", false),
+        Arguments.of("String value", "some value", true),
+        Arguments.of("Other value", 42, true));
   }
 
-  @Test
-  public void isSetShouldDetectEmptyString() {
-    assertThat(FlusswerkConfiguration.isSet("")).isFalse();
-  }
-
-  @ParameterizedTest(name = "{index} value=\"{0}\"")
-  @ValueSource(strings = {" ", "\\n", "  \\t\\n "})
-  public void isSetShouldDetectWhitespace(String string) {
-    // String replace for JUnit output
-    String value = string.replace("\\n", "\n").replace("\\t", "\t");
-    assertThat(FlusswerkConfiguration.isSet(value)).isFalse();
-  }
-
-  @Test
-  public void isSetShouldDetectValue() {
-    assertThat(FlusswerkConfiguration.isSet("name")).isTrue();
-  }
-
-  @Test
-  public void isSetShouldAcceptNonStrings() {
-    assertThat(FlusswerkConfiguration.isSet(42)).isTrue();
+  @ParameterizedTest(name = "{0} should be {2}")
+  @DisplayName("detect if value is set")
+  @MethodSource("isSetSource")
+  void isSetShouldDetectIfValueIsSet(String label, Object value, boolean expected) {
+    assertThat(FlusswerkConfiguration.isSet(value)).isEqualTo(expected);
   }
 }
