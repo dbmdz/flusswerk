4ba838b7c5d568bc4422e120dea493d654072a39
Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
2020-06-17 17:16:54 +0200

    Simplify example

--- examples-spring-boot/src/main/java/com/github/dbmdz/flusswerk/spring/boot/example/Application.java
+++ examples-spring-boot/src/main/java/com/github/dbmdz/flusswerk/spring/boot/example/Application.java
@@ -1,11 +1,8 @@
 package com.github.dbmdz.flusswerk.spring.boot.example;
 
-import com.github.dbmdz.flusswerk.framework.config.properties.FlusswerkProperties;
-import com.github.dbmdz.flusswerk.framework.engine.Engine;
 import com.github.dbmdz.flusswerk.framework.EnableFlusswerk;
 import com.github.dbmdz.flusswerk.framework.FlusswerkApplication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.github.dbmdz.flusswerk.framework.engine.Engine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,15 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableFlusswerk
 public class Application extends FlusswerkApplication {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
-
   @Autowired
-  public Application(FlusswerkProperties flusswerkProperties, Engine engine) {
+  public Application(Engine engine) {
     super(engine);
-    var connection = flusswerkProperties.getConnection();
-    LOGGER.info(
-        "Creating Flusswerk application for {}:{}",
-        connection.getHost(), connection.getPort());
   }
 
   public static void main(String[] args) {
