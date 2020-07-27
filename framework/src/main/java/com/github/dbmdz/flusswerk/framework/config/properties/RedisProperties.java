f8978a05676155049944fe5a0fbe3ee371a7c69d
Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
2020-07-27 13:59:20 +0200

    Fix redis configuration

--- framework/src/main/java/com/github/dbmdz/flusswerk/framework/config/properties/RedisProperties.java
+++ framework/src/main/java/com/github/dbmdz/flusswerk/framework/config/properties/RedisProperties.java
@@ -8,6 +8,8 @@ import org.springframework.util.StringUtils;
 @ConfigurationProperties(prefix = "flusswerk.redis")
 public class RedisProperties {
 
+  private static final int DEFAULT_PORT = 6379;
+
   private final String address;
   private final String password;
 
@@ -18,11 +20,20 @@ public class RedisProperties {
       return;
     }
 
-    if (!(address.startsWith("redis://") || address.startsWith("rediss://"))) {
-      throw new RuntimeException("Redis connection string has to start with redis:// or rediss://");
+    if (address.startsWith("redis://") || address.startsWith("rediss://")) {
+      if (address.substring(8).contains(":")) {
+        this.address = address;
+      } else {
+        this.address = address + ":" + DEFAULT_PORT;
+      }
+    } else {
+      if (address.contains(":")) {
+        this.address = "redis://" + address;
+      } else {
+        this.address = "redis://" + address + ":" + DEFAULT_PORT;
+      }
     }
 
-    this.address = address;
     this.password = password; // might be null if no authentication is used
   }
 
--- framework/src/test/java/com/github/dbmdz/flusswerk/framework/config/properties/RedisPropertiesTest.java
+++ framework/src/test/java/com/github/dbmdz/flusswerk/framework/config/properties/RedisPropertiesTest.java
@@ -12,6 +12,20 @@ import org.junit.jupiter.params.provider.ValueSource;
 @DisplayName("The RedisProperties")
 class RedisPropertiesTest {
 
+  @DisplayName("should normalize addresses")
+  @ParameterizedTest
+  @CsvSource({
+    "localhost,redis://localhost:6379",
+    "redis://localhost,redis://localhost:6379",
+    "localhost:1234,redis://localhost:1234",
+    "rediss://localhost,rediss://localhost:6379",
+    "rediss://localhost:1234,rediss://localhost:1234"
+  })
+  void shouldNormalizeAddresses(String address, String expected) {
+    var properties = new RedisProperties(address, null);
+    assertThat(properties.getAddress()).isEqualTo(expected);
+  }
+
   @DisplayName("should allow not setting address")
   @ParameterizedTest(name = "name=\"{0}\"")
   @NullSource
