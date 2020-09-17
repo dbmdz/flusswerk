7e11ce4100430f572b30867388e15b4874ef646b
Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
2020-09-17 09:59:36 +0200

    Document Engine API

--- framework/src/main/java/com/github/dbmdz/flusswerk/framework/engine/NoOpEngine.java
+++ framework/src/main/java/com/github/dbmdz/flusswerk/framework/engine/NoOpEngine.java
@@ -10,17 +10,25 @@ public class NoOpEngine implements Engine {
 
   private static final EngineStats ENGINE_STATS = new EngineStats(0, 0, 0);
 
+  /** Start fails with an exception because a no-op engine is supposed to not do any processing. */
   @Override
   public void start() {
     throw new RuntimeException("Cannot start engine. Did you define a Flow bean?");
   }
 
+  /** Does nothing as expected. */
   @Override
   public void process(Message message) {}
 
+  /** Does nothing as no processing would ever happen. */
   @Override
   public void stop() {}
 
+  /**
+   * Correctly reports the processing of nothing.
+   *
+   * @return processing information reporting that no processing has happened.
+   */
   @Override
   public EngineStats getStats() {
     return ENGINE_STATS;
