1ef02d8c8c3b64ab04a604628eb3798aadab62cd
Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
2020-10-01 14:32:49 +0200

    Add improved process report
    The new StructuredProcessReport will add useful fields for structured
    logging and remove unhelpful information. Am new Spring logging
    configuration will support the use of structured logging right away.

---
+++ framework/src/main/java/com/github/dbmdz/flusswerk/framework/reporting/StructuredProcessReport.java
@@ -0,0 +1,69 @@
+package com.github.dbmdz.flusswerk.framework.reporting;
+
+import static java.util.Objects.requireNonNull;
+import static net.logstash.logback.argument.StructuredArguments.keyValue;
+
+import com.github.dbmdz.flusswerk.framework.exceptions.StopProcessingException;
+import com.github.dbmdz.flusswerk.framework.model.Envelope;
+import com.github.dbmdz.flusswerk.framework.model.Message;
+import java.time.format.DateTimeFormatter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StructuredProcessReport implements ProcessReport {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(DefaultProcessReport.class);
+
+  private final String name;
+
+  public StructuredProcessReport(String name) {
+    this.name = requireNonNull(name);
+  }
+
+  @Override
+  public void reportSuccess(Message message) {
+    // should be default, so no need to log anything and spam the logs
+  }
+
+  @Override
+  public void reportFail(Message message, StopProcessingException e) {
+    Envelope envelope = message.getEnvelope();
+    LOGGER.error(
+        "{} failed, will not retry",
+        name,
+        keyValue("status", "stop"),
+        keyValue("incomingQueue", envelope.getSource()),
+        keyValue("retries", envelope.getRetries()),
+        keyValue("timestamp", envelope.getTimestamp().format(DateTimeFormatter.ISO_DATE_TIME)),
+        keyValue("tracingId", message.getTracingId()),
+        e);
+  }
+
+  @Override
+  public void reportFailAfterMaxRetries(Message message, Exception e) {
+    Envelope envelope = message.getEnvelope();
+    LOGGER.error(
+        "{} failed after max retries",
+        name,
+        keyValue("status", "spent"),
+        keyValue("incomingQueue", envelope.getSource()),
+        keyValue("retries", envelope.getRetries()),
+        keyValue("timestamp", envelope.getTimestamp().format(DateTimeFormatter.ISO_DATE_TIME)),
+        keyValue("tracingId", message.getTracingId()),
+        e);
+  }
+
+  @Override
+  public void reportReject(Message message, Exception e) {
+    Envelope envelope = message.getEnvelope();
+    LOGGER.error(
+        "{} failed, but will retry later",
+        name,
+        keyValue("status", "retry"),
+        keyValue("incomingQueue", envelope.getSource()),
+        keyValue("retries", envelope.getRetries()),
+        keyValue("timestamp", envelope.getTimestamp().format(DateTimeFormatter.ISO_DATE_TIME)),
+        keyValue("tracingId", message.getTracingId()),
+        e);
+  }
+}
