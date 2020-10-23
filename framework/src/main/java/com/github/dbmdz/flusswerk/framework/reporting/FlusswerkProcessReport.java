d9019af7dee739560e1e9a8588bc6e10b38f427f
Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
2020-10-23 17:48:25 +0200

    Add new FlusswerkLogger

---
+++ framework/src/main/java/com/github/dbmdz/flusswerk/framework/reporting/FlusswerkProcessReport.java
@@ -0,0 +1,63 @@
+package com.github.dbmdz.flusswerk.framework.reporting;
+
+import static java.util.Objects.requireNonNull;
+import static net.logstash.logback.argument.StructuredArguments.keyValue;
+
+import com.github.dbmdz.flusswerk.framework.exceptions.StopProcessingException;
+import com.github.dbmdz.flusswerk.framework.model.Envelope;
+import com.github.dbmdz.flusswerk.framework.model.Message;
+
+public class FlusswerkProcessReport implements ProcessReport {
+
+  protected final FlusswerkLogger logger;
+  protected final String name;
+
+  public FlusswerkProcessReport(String name, FlusswerkLogger logger) {
+    this.name = requireNonNull(name);
+    this.logger = requireNonNull(logger);
+  }
+
+  @Override
+  public void reportSuccess(Message message) {
+    // should be default, so no need to log anything and spam the logs
+  }
+
+  @Override
+  public void reportFail(Message message, StopProcessingException e) {
+    Envelope envelope = message.getEnvelope();
+    logger.error(
+        "{} failed, will not retry",
+        name,
+        keyValue("will_retry", false),
+        keyValue("incoming_queue", envelope.getSource()),
+        keyValue("retries", envelope.getRetries()),
+        keyValue("tracing_id", message.getTracingId()),
+        e);
+  }
+
+  @Override
+  public void reportFailAfterMaxRetries(Message message, Exception e) {
+    Envelope envelope = message.getEnvelope();
+    logger.error(
+        "{} failed after max retries",
+        name,
+        keyValue("will_retry", false),
+        keyValue("incoming_queue", envelope.getSource()),
+        keyValue("retries", envelope.getRetries()),
+        keyValue("tracing_id", message.getTracingId()),
+        e);
+  }
+
+  @Override
+  public void reportReject(Message message, Exception e) {
+    Envelope envelope = message.getEnvelope();
+    logger.error(
+        "{} failed, but will retry later",
+        name,
+        keyValue("will_retry", true),
+        keyValue("incoming_queue", envelope.getSource()),
+        keyValue("retries", envelope.getRetries()),
+        keyValue("tracing_id", message.getTracingId()),
+        e);
+  }
+}
---
+++ framework/src/test/java/com/github/dbmdz/flusswerk/framework/reporting/FlusswerkLoggerTest.java
@@ -0,0 +1,74 @@
+package com.github.dbmdz.flusswerk.framework.reporting;
+
+import static net.logstash.logback.argument.StructuredArguments.keyValue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+
+@DisplayName("The FlusswerkLogger")
+class FlusswerkLoggerTest {
+
+  private Logger logger;
+  private FlusswerkLogger flusswerkLogger;
+  private Tracing tracing;
+
+  @BeforeEach
+  void setUp() {
+    logger = mock(Logger.class);
+    tracing = mock(Tracing.class);
+    flusswerkLogger = new FlusswerkLogger(logger, tracing);
+  }
+
+  @DisplayName("should use error logging")
+  @Test
+  void error() {
+    flusswerkLogger.error("format string", "a");
+    verify(logger).error(anyString(), (Object[]) any());
+  }
+
+  @DisplayName("should use warn logging")
+  @Test
+  void warn() {
+    flusswerkLogger.warn("format string", "a");
+    verify(logger).warn(anyString(), (Object[]) any());
+  }
+
+  @DisplayName("should use info logging")
+  @Test
+  void info() {
+    flusswerkLogger.info("format string", "a");
+    verify(logger).info(anyString(), (Object[]) any());
+  }
+
+  @DisplayName("should use debug logging")
+  @Test
+  void debug() {
+    flusswerkLogger.debug("format string", "a");
+    verify(logger).debug(anyString(), (Object[]) any());
+  }
+
+  @DisplayName("should use trace logging")
+  @Test
+  void trace() {
+    flusswerkLogger.trace("format string", "a");
+    verify(logger).trace(anyString(), (Object[]) any());
+  }
+
+  @DisplayName("should add tracing information")
+  @Test
+  void addTracing() {
+    when(tracing.tracingPath()).thenReturn(List.of("TRACING"));
+    Object[] a = {"A"};
+    Object[] actual = flusswerkLogger.addTracing(a);
+    assertThat(actual).containsExactly("A", keyValue("tracing", tracing.tracingPath()));
+  }
+}
