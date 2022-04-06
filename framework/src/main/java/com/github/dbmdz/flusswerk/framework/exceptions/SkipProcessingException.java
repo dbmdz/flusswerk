bdefa36f2c74448689f04e6a4af5798d74821e22
Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
2022-04-06 15:54:27 +0200

    Add missing methods for SkipProcessingException

--- framework/src/main/java/com/github/dbmdz/flusswerk/framework/exceptions/SkipProcessingException.java
+++ framework/src/main/java/com/github/dbmdz/flusswerk/framework/exceptions/SkipProcessingException.java
@@ -7,11 +7,18 @@ import com.github.dbmdz.flusswerk.framework.model.Message;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 /** Indicates that processing should be skipped from the point on where this exception is thrown. */
 public class SkipProcessingException extends RuntimeException {
 
   private final List<Message> outgoingMessages = new ArrayList<>();
+
+  private SkipProcessingException(String message, Throwable cause, List<Message> outgoingMessages) {
+    super(message, cause);
+    this.outgoingMessages.addAll(outgoingMessages);
+  }
 
   /**
    * Skip processing with an explanation of why.
@@ -48,5 +55,27 @@ public class SkipProcessingException extends RuntimeException {
    */
   public List<Message> getOutgoingMessages() {
     return unmodifiableList(outgoingMessages);
+  }
+
+  /**
+   * Add the exceptions cause to the exception without interfering with the format string arguments.
+   *
+   * @param cause The cause to add
+   * @return An augmented version of the exception including the cause
+   */
+  public SkipProcessingException causedBy(Throwable cause) {
+    return new SkipProcessingException(getMessage(), cause, outgoingMessages);
+  }
+
+  /**
+   * Fluid interface to create exception suppliers, e.g. for use in {@link
+   * Optional#orElseThrow(Supplier)}.
+   *
+   * @param message The message, possibly including format strings
+   * @param args Arguments for the format string in <code>message</code>
+   * @return A supplier for this exception than can be further customized
+   */
+  public static ExceptionSupplier<SkipProcessingException> because(String message, Object... args) {
+    return new ExceptionSupplier<>(SkipProcessingException.class, message, args);
   }
 }
